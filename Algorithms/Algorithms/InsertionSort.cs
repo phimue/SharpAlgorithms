4584ebec757d4f47adae0e8451f813b9f69540b8
phimue <philippmueller02@web.de>
2025-05-17 21:40:42 +0200

    Added insertion sort

---
+++ Algorithms/Algorithms/InsertionSort.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms
+{
+    internal class InsertionSort
+    {
+        int[] A { get; set; }
+
+        internal InsertionSort(int[] a) 
+        {
+            this.A = a;
+        }
+
+        internal int[] Sort()
+        {
+            int[] a = new int[A.Length];
+            A.CopyTo(a, 0);
+
+            for(int j = 1; j < a.Length; j++)
+            {
+                int key = a[j];
+                int i = j - 1;
+                while (i >= 0 && a[i] > key)
+                {
+                    a[i + 1] = a[i];
+                    i = i - 1;
+                }
+                a[i+1] = key;
+            }
+
+            return a;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms;
+
+int[] a = [5, 2, 4, 6, 1, 3];
+
+InsertionSort insertionSort = new InsertionSort(a);
+
+foreach(int i in a)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+int[] b = insertionSort.Sort();
+
+foreach(int j in b)
+{
+    Console.WriteLine(j);
+}
